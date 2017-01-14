c889567bb6b178563c1750b1c59d12e1639e0d1c
yehmoonblue <moonblue.yeh@gmail.com>
2017-01-14 17:29:20 +0800

    Update m.js

--- js/m.js
+++ js/m.js
@@ -1,3 +1,6 @@
 console.log(123)
 $("#selectList").val();
 $("#selectList :selected").text();
+$("#test1").change(function(){
+  $("#test2").addOption($(this).val(), $('#test1 :selected').text());  
+});
