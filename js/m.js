733a89b8ec504d1446f3f7ea57ee3c33e2931fe7
yehmoonblue <moonblue.yeh@gmail.com>
2017-01-21 17:20:41 +0800

    Update m.js

--- js/m.js
+++ js/m.js
@@ -1,4 +1,15 @@
 console.log(123)
+function check(){
+            var talk=true;
+            var name = $("input#name").val();
+            var age = $("input#age").val();
+            if(talk){
+                        alert("Hi!,"+$("input#name").val("")+",你今年是"+$("input#age").val("")+"歲。")
+            };
+            
+            
+            
+            
 $("selector").val();
 $("body").on("click","#button", function(){
             if( $("#one-checkbox").prop("checked") ){
