945f6b966deb373a8b15f48f0c70ff4dbb6e4b05
yehmoonblue <moonblue.yeh@gmail.com>
2017-01-21 16:13:50 +0800

    Update m.js

--- js/m.js
+++ js/m.js
@@ -5,17 +5,12 @@ function check(){
             var name = $("input#name").val();
             var age = $("input#age").val();
             if(talk){
-            alert("Hi!,"+name+",你今年是"+age+"歲。")}
-            
+                        alert("Hi!,"+$("input#name").val("")+",你今年是"+$("input#age").val("")+"歲。")
+            };
+            $("body").on("click","#button", function(){
+            if( $("#one-checkbox").prop("checked") ){
+                        alert( "你是"+$("#one-checkbox").val());
+            } else {
+                        alert( "你不是"+$("#one-checkbox").val() );
             }
-$("input#name").val("");
-$("input#age").val("");
-
-
-$("body").on("click","#button", function(){
-        if( $("#one-checkbox").prop("checked") ){ // 回傳布林值
-            alert( "你是"+$("#one-checkbox").val() );
-        } else {
-            alert( "你不是"+$("#one-checkbox").val() );
-        }
-    });
+});
